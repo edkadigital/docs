fcfc64bd648358fadab281dc1eae4ac4e68cb130
Camil Blanaru <camil@edka.io>
2025-05-17 21:04:30 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -14,16 +14,16 @@ export default [
   },
   js.configs.recommended,
   ...tseslint.configs.recommended, // For general TS files
-  
+
   // Astro specific configuration
-  ...eslintPluginAstro.configs['flat/recommended'],
+  ...eslintPluginAstro.configs["flat/recommended"],
   // You can add further overrides for Astro files if needed, for example:
   {
     files: ["**/*.astro"],
     rules: {
       // Example: "@typescript-eslint/no-unused-vars": "warn"
       // Add any Astro-specific rule overrides here
-    }
+    },
   },
 
   // Global settings for JS/TS files (excluding Astro components' script tags handled by the above)
